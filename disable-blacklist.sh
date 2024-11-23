bdc478ce2215eb1e0dba31e35c19338febe3d9bf
Astatoru <astatoru@gmail.com>
2024-11-23 13:35:03 +0300

    update

--- disable-blacklist.sh
+++ disable-blacklist.sh
@@ -5,10 +5,14 @@ if [ $(id -u) -ne 0 ]
   exit
 fi
 
+# Backup current iptables rules
 if [ ! -d "iptables-backup" ]; then
   mkdir "iptables-backup"
 fi
-iptables-save > "iptables-backup/iptables_disable-blacklist.rules"
+if [ -f "iptables-backup/iptables.rules" ]; then
+	mv "iptables-backup/iptables.rules" "iptables-backup/iptables_old.rules"
+fi
+iptables-save > "iptables-backup/iptables.rules"
 
 if iptables -C INPUT -m set --match-set ru-blacklist src -j DROP 2>/dev/null; then
 	iptables -D INPUT -m set --match-set ru-blacklist src -j DROP
--- enable-iptables-rules.sh
+++ enable-iptables-rules.sh
@@ -9,11 +9,16 @@ fi
 if [ ! -d "iptables-backup" ]; then
   mkdir "iptables-backup"
 fi
-iptables-save > "iptables-backup/iptables_enable-iptables-rules.rules"
+if [ -f "iptables-backup/iptables.rules" ]; then
+	mv "iptables-backup/iptables.rules" "iptables-backup/iptables_old.rules"
+fi
+iptables-save > "iptables-backup/iptables.rules"
 
 # SSH
-if ! iptables -C INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT 2>/dev/null; then
-	iptables -I INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+read -p "Enter the ssh port number to add the rule (default 22): " SSH_PORT
+SSH_PORT=${SSH_PORT:-22}
+if ! iptables -C INPUT -p tcp --dport $SSH_PORT -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT 2>/dev/null; then
+	iptables -A INPUT -p tcp --dport $SSH_PORT -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 fi
 
 # Local interface
@@ -35,8 +40,13 @@ if ! iptables -C INPUT -p tcp -m multiport --dports 80,443,8443 -m conntrack --c
 fi
 
 # X-UI web panel
-if ! iptables -C INPUT -p tcp --dport 11111 -j ACCEPT 2>/dev/null; then
-	iptables -A INPUT -p tcp --dport 11111 -j ACCEPT
+read -p "Enter the X-UI web panel port number to add the rule (empty if none): " XUI_PORT
+if [ -n "$XUI_PORT" ]; then
+    if ! iptables -C INPUT -p tcp --dport "$XUI_PORT" -j ACCEPT 2>/dev/null; then
+        iptables -A INPUT -p tcp --dport "$XUI_PORT" -j ACCEPT
+    fi
+else
+    echo "No port entered, skipping rule addition for X-UI web panel"
 fi
 
 # Default policies
@@ -48,4 +58,4 @@ if [ ! -d "/etc/iptables" ]; then
   mkdir "/etc/iptables"
 fi
 iptables-save > /etc/iptables/iptables.rules
-echo "Iptables rules were applied"
+echo "iptables rules were successfully applied"
