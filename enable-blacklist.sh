40c2f53a6b60e25bdf635946b870fbf08bb355e6
Astatoru <astatoru@gmail.com>
2024-11-24 01:01:38 +0300

    Update enable-blacklist.sh

--- enable-blacklist.sh
+++ enable-blacklist.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
 if [ $(id -u) -ne 0 ]
   then echo "Please run this script as root or using sudo"
-  exit
+  exit 1
+fi
+
+if [[ ! -d "/etc/iptables/" ]]; then
+	echo "The script is intended to be used with iptables. Are you sure all the necessary packages are installed?"
+	exit 2
+fi
+
+if [[ ! -d "/etc/ipset.conf" ]]; then
+	echo "The script is intended to be used with ipset. Are you sure all the necessary packages are installed?"
+	exit 2
 fi
 
 # Backup current iptables rules
@@ -30,12 +41,11 @@ while read -r ip; do
 		echo "$ip was successfully added to ipset"
     fi
 done < ru-blacklist.txt
-
 ipset save > /etc/ipset.conf
 
 if ! iptables -n -t raw -C PREROUTING -m set --match-set ru-blacklist src -j DROP 2>/dev/null; then
 	iptables -n -t raw -I PREROUTING -m set --match-set ru-blacklist src -j DROP
-	echo "The rule for INPUT chain was successfully created"
+	echo "The rule for PREROUTING chain was successfully created"
 fi
 
 if [ ! -d "/etc/iptables" ]; then
